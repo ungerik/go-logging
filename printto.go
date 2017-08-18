a636838b4ce4d7c384087198b181ac17e1dd5ac3
Erik Unger <erik@erikunger.com>
2017-08-18 23:32:34 +0200

    add mutex

--- printto.go
+++ printto.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sync"
 )
 
 var DefaultLogger Logger = NewPrintTo(os.Stderr, true, DefaultFormatter)
@@ -12,21 +13,26 @@ type PrintTo struct {
 	writer    io.Writer
 	debug     bool
 	formatter Formatter
+	mutex     sync.Mutex
 }
 
 func NewPrintTo(writer io.Writer, debug bool, formatter Formatter) *PrintTo {
 	if formatter == nil {
 		formatter = DefaultFormatter
 	}
-	return &PrintTo{writer, debug, formatter}
+	return &PrintTo{writer: writer, debug: debug, formatter: formatter}
 }
 
 func (printTo *PrintTo) Printf(format string, args ...interface{}) {
+	printTo.mutex.Lock()
+	defer printTo.mutex.Unlock()
 	fmt.Fprintln(printTo.writer, printTo.formatter.Format(format, args...))
 }
 
 func (printTo *PrintTo) Debugf(format string, args ...interface{}) {
 	if printTo.debug {
+		printTo.mutex.Lock()
+		defer printTo.mutex.Unlock()
 		fmt.Fprintln(printTo.writer, printTo.formatter.Format(format, args...))
 	}
 }
